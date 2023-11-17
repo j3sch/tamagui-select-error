c9d7199bc271f95d5cfab4a54d976c9f0e32763f
Jens Schlegel <39447994+j3sch@users.noreply.github.com>
2023-11-17 14:08:17 +0100

    feat: add select

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -9,6 +9,7 @@ import {
   XStack,
   YStack,
 } from '@my/ui'
+import { SelectDemo } from '@my/ui/src/Select'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
@@ -20,30 +21,7 @@ export function HomeScreen() {
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600}>
-        <H1 ta="center">Welcome to Tamagui.</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
-
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
-          <Anchor
-            color="$color12"
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            give it a ⭐️
-          </Anchor>
-        </Paragraph>
-      </YStack>
+      <SelectDemo />
 
       <XStack>
         <Button {...linkProps}>Link to user</Button>
